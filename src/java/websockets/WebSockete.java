1f316f3ba6f06fb47b4626e63eb0890fa490df2d
Luis Marin <la.marin@outlook.com>
2023-03-27 16:49:15 -0700

    Minimal Setup
    Configuring MAIN

--- src/java/websockets/WebSockete.java
+++ src/java/websockets/WebSockete.java
@@ -14,12 +14,12 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
-import jakarta.websocket.OnClose;
-import jakarta.websocket.OnError;
-import jakarta.websocket.OnMessage;
-import jakarta.websocket.OnOpen;
-import jakarta.websocket.Session;
-import jakarta.websocket.server.ServerEndpoint;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
 
 /**
  *
